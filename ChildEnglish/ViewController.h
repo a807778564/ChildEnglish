e1de4f26a6b389e82f6680de2a6a9d938488701e
huangrensheng <huangrensheng@qutoutiao.net>
2018-05-04 11:06:14 +0800

    Initial Commit

---
+++ ChildEnglish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChildEnglish
+//
+//  Created by huangrensheng on 2018/5/4.
+//  Copyright © 2018年 huangrensheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChildEnglish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChildEnglish
+//
+//  Created by huangrensheng on 2018/5/4.
+//  Copyright © 2018年 huangrensheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
